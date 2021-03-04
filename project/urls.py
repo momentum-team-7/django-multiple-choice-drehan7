382d13f24eba4a4c9cce8aa2fe116e90b9b535cf
mjbiener <michaelbiener@yahoo.com>
2021-03-04 11:34:29 -0500

    created index page added url and view info

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
+from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
 
 if settings.DEBUG:
