0105fb31ca646aff2035a9de9f9ee6f09be7ced1
mjbiener <michaelbiener@yahoo.com>
2021-03-04 13:56:55 -0500

    added User registration steps

--- project/urls.py
+++ project/urls.py
@@ -20,6 +20,7 @@ from core import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('registration.backends.simple.urls')),
     path('', include('core.urls')),
 ]
 
