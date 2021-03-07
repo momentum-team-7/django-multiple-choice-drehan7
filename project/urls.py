0e044380cd2f8b881db513c4ecbe8fdaf4093634
mjbiener <michaelbiener@yahoo.com>
2021-03-07 13:38:01 -0500

    added profile images to profile page

--- project/urls.py
+++ project/urls.py
@@ -17,12 +17,14 @@ from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
     path('', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
